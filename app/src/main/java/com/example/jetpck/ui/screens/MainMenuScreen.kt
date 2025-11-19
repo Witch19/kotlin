afd380b600ac34099be77d34ae6a2b29fc0d91f9
JOSS COMPU <josytst.js@gmail.com>
2025-11-19 17:48:43 -0500

    Modificacion

--- app/src/main/java/com/example/jetpck/ui/screens/MainMenuScreen.kt
+++ app/src/main/java/com/example/jetpck/ui/screens/MainMenuScreen.kt
@@ -1,18 +1,45 @@
-package com.example.jetpck
+package com.example.jetpck.ui.screens
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import com.example.jetpck.ui.navigation.AppNavigation
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.jetpck.ui.navigation.AppScreen
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            androidx.compose.material3.MaterialTheme {
-                AppNavigation()
-            }
+@Composable
+fun MainMenuScreen(nav: NavController) {
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(25.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text("Mi Día Inteligente", style = MaterialTheme.typography.headlineMedium)
+        Text("Cálculos rápidos del día")
+
+        Spacer(Modifier.height(30.dp))
+
+        Button(onClick = { nav.navigate(AppScreen.Hidratacion.route) }) {
+            Text("Hidratación diaria")
         }
 
+        Spacer(Modifier.height(12.dp))
+
+        Button(onClick = { nav.navigate(AppScreen.Estudio.route) }) {
+            Text("Estudio semanal")
+        }
+
+        Spacer(Modifier.height(12.dp))
+
+        Button(onClick = { nav.navigate(AppScreen.Transporte.route) }) {
+            Text("Transporte mensual")
+        }
     }
 }
+
